d71ccbcaa3b84d171c3b2fa092e8062c6f9d0a01
WobblyAnt8 <slavyan_ut@mail.ru>
2018-11-11 22:19:57 +0300

    Написана функция AphabetSort, Добавлен конструктор копирования, перегружена операция =

--- SIGN.cpp
+++ SIGN.cpp
@@ -1,43 +1,58 @@
-#include "stdafx.h"
-#include "SIGN.h"
+include "SIGN.h"
+
 using namespace std;
+
 SIGN::SIGN()
 {
 	cout << "ctor" << endl;
 	int i = 0;
-	cout << "Enter name of the Name: ";
+	cout << "Enter name of the : ";
 	cin >> Name;
-
+	
 	cout << "Enter name of the : ";
 	cin >> Surname;
 	cout << "Enter the SIGN:";
 	cin >> DATA;
 }
+SIGN::SIGN(const SIGN*)
+{
+	cout << "copy ctor" << endl;
+	this->Name = Name;
+	this->Surname = Surname;
+	this->DATA = DATA;
+}
 SIGN :: ~SIGN()
 {
 	cout << "dtor" << endl;
-	//free(Name);
-	//free(Surname);
-}
-void SIGN::show()
-{
-	cout << "Name of the Name:" << Name << endl;
-	cout << "Name of the :" << Surname << endl;
-	cout << "SIGN:" << DATA << endl;
-}
-string SIGN::getName()
-{
-	return Name;
-}
-string SIGN::getSurname()
-{
-	return Surname;
-}
-void SIGN::Show()
-{
-	cout << "Name of the :" << Surname << endl;
-}
-void SIGN::ShowDATA()
-{
-	cout << "SIGN:" << DATA << endl;
-}
+	@@ - 31, 24 + 41, 32 @@
+
+		void SIGN::show()
+	{
+		cout << "Name of the :" << Name << endl;
+		cout << "Name of the :" << Surname << endl;
+		cout << "SIGN:" << DATA << endl;
+	}
+
+	string SIGN::Name()
+	{
+		return Name;
+	}
+	string SIGN::getSurname()
+	{
+		return Surname;
+	}
+	void SIGN::Show()
+	{
+		cout << "Name of the :" << Surname << endl;
+	}
+	void SIGN::ShowDATA()
+	{
+		cout << "SIGN:" << DATA << endl;
+	}
+	SIGN* SIGN::operator=(SIGN*)
+	{
+		this->Surname = Surname;
+		this->Name = Name;
+		this->DATA = DATA;
+		return this;
+	}
